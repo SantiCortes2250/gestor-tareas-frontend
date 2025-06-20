c0a010d88b7abb2e1172dd4628e8c154d02205b9
SantiCortes2250 <santicortesrincon15@gmail.com>
2025-06-20 21:36:59 +0200

    ready to deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/pages/Home.vue'
-import Login from '@/pages/Login.vue'
-import Register from '@/pages/Register.vue'
+import Home from '../Pages/Home.vue'
+import Login from '../Pages/Login.vue'
+import Register from '../Pages/Register.vue'
 
 
 const routes = [
